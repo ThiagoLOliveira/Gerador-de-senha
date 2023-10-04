9aa69d487ba39bb897c2753933e42d416a99f309
Thiago Lima de Oliveira <113208530+ThiagoLOliveira@users.noreply.github.com>
2023-10-04 18:32:27 -0300

    Update main.py
    Adicionado uma criptografia nas senhas, e um banco de dados nas fases iniciais, esta dando alguns erros, porem no proximo commit pretendo ja deixar ajustado

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import random as rd
 import string
+import sqlite3 as sql
+import hashlib as hlib
+import os
 
 print('=*'*15)
 print("Bem vindo ao gerador de Senhas")
@@ -16,7 +19,76 @@ alfabeto_mai = string.ascii_uppercase
 lista_letra_min = list(alfabeto_min)
 lista_letra_mai = list(alfabeto_mai)
 lista_numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-senha = []
+
+
+def cria_hash(senhaCrip, salt=None):
+    # Cria um salt com um valor de 126 bytes 16bits
+    if salt is None:
+        salt = os.urandom(16)
+
+    # Junta a senha mais o salt gerado pelo sistema
+    senha_salt = senhaCrip.encode('utf-8') + salt
+
+    # Cria a criptografia
+    sha256 = hlib.sha256()
+    sha256.update(senha_salt)
+    hash_senha = sha256.digest()
+
+
+    return salt, hash_senha
+
+def criar_tabela_usuarios():
+    conexao = sql.connect('senha_criptografadas.db')
+    cursor = conexao.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Usuarios (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        SenhaHash TEXT)''')
+    conexao.commit()
+    conexao.close()
+
+#Banco de dados das senhas
+def guardar_senha(senha):
+    bd_senha = sql.connect('senha_criptografadas.db')
+    cursor = bd_senha.cursor()
+    guardar_sql = f"INSERT INTO Usuarios(SenhaHash) VALUES(?)"
+    cursor.execute(guardar_sql, (senha))
+    bd_senha.commit()
+
+
+def verificar_dados(bd, dados = False):
+    if dados:
+        conexao = sql.connect(bd)
+        cursor = conexao.cursor()
+        cursor.execute("SELECT * FROM Usuarios")
+        registros = cursor.fetchall()
+        for registro in registros:
+            print(registro)
+        conexao.close()
+
+
+#Gera a senha e armazena
+def gerar_senha():
+    senha = []
+    for i in range(qtd):
+        # Gera um numero para sortear o valor que sera usado como senha
+        tipo_caractere = rd.randint(1, 4)
+        if tipo_caractere == 1:
+            senha.append(rd.choice(lista_letra_min))
+        elif tipo_caractere == 2:
+            senha.append(rd.choice(lista_letra_mai))
+        elif tipo_caractere == 3:
+            senha.append(str(rd.choice(lista_numeros)))
+        else:
+            senha.append(rd.choice(especiais))
+    
+    senha_texto = ''.join(senha)
+    rd.shuffle(senha_texto)
+
+    salt, hash_senha = cria_hash(senha_texto)
+    guardar_senha(hash_senha)
+
+
+criar_tabela_usuarios()
 
 while True:
     qtd = int(input('\nDeseja uma senha de quantos digitos?:'))
@@ -24,21 +96,5 @@ while True:
         break
     else:
         print('A senha deve ter entre 8 e 20 digitos')
-
-
-for i in range(qtd):
-    tipo_caractere = rd.randint(1, 4)
-
-    if tipo_caractere == 1:
-        senha.append(rd.choice(lista_letra_min))
-    elif tipo_caractere == 2:
-        senha.append(rd.choice(lista_letra_mai))
-    elif tipo_caractere == 3:
-        senha.append(str(rd.choice(lista_numeros)))
-    else:
-        senha.append(rd.choice(especiais))
-
-rd.shuffle(senha)
-print("Sua senha é: ", end='')
-for i in senha:
-    print(i, end='')
+        
+verificar_dados('senha_criptografadas.db', dados=True)
